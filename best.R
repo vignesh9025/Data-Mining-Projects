da246cfe7c983d5f9a0f47343b4e80060ebbf141
Vignesh M.P <vignesh9025@yahoo.com>
2016-08-15 01:09:52 +0530

    COMMIT 2 - ALG - MINE DATA FROM HOSP. DATA SET TO FIND BEST PER STATE (US)

--- best.R
+++ best.R
@@ -1,31 +1,51 @@
 best <- function(state, on)
 {
       outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-      
+
       ## Attributes: [2], [7], [11]
       
       outcome <- outcome[complete.cases(outcome), ]
       
       outcome <- outcome[, c(2, 7, 11, 17, 23)]
+      
       if(on == "heart attack")
       {
             on <- paste("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", sep = "")
       }
-      if(on == "heart failure")
+      else if(on == "heart failure")
       {
             on <- paste("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", sep = "")
       }
-      if(on == "pneumonia")
+      else if(on == "pneumonia")
       {
             on <- paste("Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia", sep = "")
+      }
+      else
+      {
+            stop("invalid outcome")
       }
       
       ## Constricting using state and on
       outcome <- outcome[outcome$State == state, c("Hospital.Name", on)]
-      outcome <- outcome[outcome[,on]!="Not Available",]
+      if(length(outcome[,"Hospital.Name"]) == 0)
+      {
+            stop("invalid state")
+      }
+      
+      outcome <- outcome[outcome[ ,on]!="Not Available", ]
       ## Computing minimum value
       
-      m <- min(as.numeric(outcome[,on]))
+      m <- min(as.numeric(outcome[ ,on]))
       
-      outcome[as.numeric(outcome[,on]) == m,"Hospital.Name"]
+      o <- outcome[as.numeric(outcome[ ,on]) == m,"Hospital.Name"]
+      if(length(o) > 1)
+      {
+            o <- o[order(o)]
+            o[1]
+      }
+      else
+      {
+            o
+      }
+      
 }
